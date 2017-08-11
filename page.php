ec4fbdb8481e3e1edc1fb829ab993b693eb2ca02
Seaton Jiang <seaton@vtrois.com>
2017-08-11 15:32:11 +0800

    Fix syntax error

--- page.php
+++ page.php
@@ -1,8 +1,4 @@
-<?php
-/*
-Template Name: 默认模版
-*/
-get_header();?>
+<?php get_header();?>
 <div id="main">
     <?php $site_banner = sirius_option('background_image');?>
     <?php if ( !empty( $site_banner ) ) {?>
@@ -12,10 +8,10 @@ get_header();?>
             <div class="container">
 				<?php if (have_posts()) : the_post(); update_post_caches($posts);
 				?>
-                <header class="post-header">
-                    <h1 class="post-title text-center"><?php the_title(); ?></h1>
-                </header>
-                <div class="post-content"><?php the_content(); ?></div>
+                    <header class="post-header">
+                        <h1 class="post-title text-center"><?php the_title(); ?></h1>
+                    </header>
+                    <div class="post-content"><?php the_content(); ?></div>
 				<?php endif; ?>
                 <?php comments_template(); ?>
             </div>
