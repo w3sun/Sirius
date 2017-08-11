27ed6988b40e7d9a2a9352c62d678e555137f6f1
Seaton Jiang <seaton@vtrois.com>
2017-08-11 15:14:37 +0800

    Fix syntax error

--- content.php
+++ content.php
@@ -4,9 +4,9 @@
 		<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
 		<div class="sirius-post-meta">
 				<span>
-				<a href="#"><i class="fa fa-calendar"></i> <?php the_time('Y/n/j') ?></a>
+				<a href="#"><i class="fa fa-calendar"></i> <?php echo get_the_date(); ?></a>
 				<?php $category = get_the_category(); echo '<a href="' . get_category_link($category[0] -> term_id) . '"><i class="fa fa-folder-open-o"></i> ' . $category[0] -> cat_name . '</a>'; ?>
-				<a href="<?php the_permalink() ?>"><i class="fa fa-eye"></i> <?php echo sirius_get_post_views(); ?> Views</a>
+				<a href="<?php the_permalink() ?>"><i class="fa fa-eye"></i> <?php echo sirius_get_post_views(); ?>人浏览</a>
 				<span>
 			</div>
 		<?php $excerptphoto = wp_trim_words(get_the_excerpt(), 110); ?>
